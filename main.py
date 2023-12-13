e367c9af4b7c9715e73c003da3917838969eb2d5
hangtruong_thinkprompt <hangttn@thinkprompt.com>
2023-12-13 15:25:01 +0700

    add record test

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from dotenv import load_dotenv
 
 from app import models
 from app.application import create_application
-from app.database.connection import SessionLocal, engine, get_db
-from app.repository.analytics_repository import ReportDisplay
+from app.database.connection import engine, get_db
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 load_dotenv(os.path.join(BASE_DIR, ".env"))
